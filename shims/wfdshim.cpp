58c7a6b5a6ee524280feb4adb7bb0834d2e2d259
Jabiyeff <cebiyevanar@gmail.com>
2022-04-06 15:22:18 +0400

    moreeee
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- shims/wfdshim.cpp
+++ shims/wfdshim.cpp
@@ -1,5 +1,6 @@
 extern "C" {
    void _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEb() {}
    void _ZN6SkData16DummyReleaseProcEPKvPv() {}
+   void _ZN8SkBitmapC1Ev() {}
    void _ZN7android11MotionEvent10initializeEiijiNSt3__15arrayIhLm32EEEiiiiiiNS_20MotionClassificationEffffffffllmPKNS_17PointerPropertiesEPKNS_13PointerCoordsE() {}
 } 
